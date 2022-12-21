6278002883b0a1d69139da55cbbb0043fb47a3cc
Syed Waqas <waqas.shah.syed@gmail.com>
2022-12-21 19:17:42 +0500

    Frontend form

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { ChangeEvent, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [numberOfEmails, setNumberOfEmails] = useState("");
+
+  const fireAwayEmails = () => {
+    
+  }
+
   return (
     <>
       <Head>
@@ -14,31 +22,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
+      <main className={"flex flex-col justify-start items-center min-h-screen"}>
         <div className={styles.center}>
           <Image
             className={styles.logo}
@@ -58,64 +42,17 @@ export default function Home() {
             />
           </div>
         </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className={"flex flex-row justify-center items-start mt-32"}>
+          <form onSubmit={fireAwayEmails}>
+          <div className='flex flex-col p-4'>
+            <p className='text-gray-300 font-semibold text-lg'>Email Address:</p>
+            <input className='rounded-md h-10 w-96 bg-gray-600' value={email} onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}></input>
+          </div>
+          <div className='flex flex-col p-4'>
+            <p className='text-gray-300 font-semibold text-lg'>Number of Emails</p>
+            <input className='rounded-md h-10 w-20 bg-gray-600' value={numberOfEmails} onChange={(e: ChangeEvent<HTMLInputElement>) => setNumberOfEmails(e.target.value)}></input>
+          </div>
+          </form>
         </div>
       </main>
     </>
